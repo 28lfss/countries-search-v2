7c41676212a7c4536d577c38f245382acf6c02db
Luiz Sa <28lfss@gmail.com>
2025-01-20 09:21:55 -0300

    fix: add length

--- models/login_form.py
+++ models/login_form.py
@@ -1,8 +1,15 @@
 from flask_wtf import FlaskForm
 from wtforms import PasswordField, SubmitField, StringField
-from wtforms.validators import DataRequired
+from wtforms.validators import DataRequired, Length
+
 
 class LoginForm(FlaskForm):
-    username = StringField('Username', validators=[DataRequired()])
-    password = PasswordField('Password', validators=[DataRequired()])
+    username = StringField(
+        'Username',
+        validators=[DataRequired(), Length(min=4, max=20)]
+    )
+    password = PasswordField(
+        'Password',
+        validators=[DataRequired(), Length(min=5, max=30)]
+    )
     submit = SubmitField('Login')
