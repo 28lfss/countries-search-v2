dbbe1edf48ec86958ea5d139c35f0c0fd1fb1d71
Luiz Sa <28lfss@gmail.com>
2025-01-22 08:44:11 -0300

    style: indentation

--- models/register_form.py
+++ models/register_form.py
@@ -3,20 +3,28 @@ from wtforms import StringField, PasswordField, SubmitField
 from wtforms.validators import DataRequired, Email, EqualTo, InputRequired, Length, ValidationError
 
 class RegisterForm(FlaskForm):
-    username = StringField(
-        'Username',
-        validators=[DataRequired(), Length(min=4, max=20)]
+    username = StringField('Username',
+        validators=[
+            DataRequired(),
+            Length(min=4, max=20)
+        ]
     )
-    email = StringField(
-        'Email',
-        validators=[DataRequired(), Email()]
+    email = StringField('Email',
+        validators=[
+            DataRequired(),
+            Email()
+        ]
     )
-    password = PasswordField(
-        'Password',
-        validators=[InputRequired(), Length(min=5, max=30)]
+    password = PasswordField('Password',
+        validators=[
+            InputRequired(),
+            Length(min=5, max=30)
+        ]
     )
-    confirm_password = PasswordField(
-        'Confirm Password',
-        validators=[DataRequired(), EqualTo('password', message='Passwords must match')]
+    confirm_password = PasswordField('Confirm Password',
+        validators=[
+            DataRequired(),
+            EqualTo('password', message='Passwords must match')
+        ]
     )
     submit = SubmitField('Register')
