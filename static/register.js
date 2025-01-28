674d93107e93c88cd2e26dff2ad1eafe36e9cc38
Luiz Sa <28lfss@gmail.com>
2025-01-27 23:07:57 -0300

    fix(register): front-end validation fix

--- static/register.js
+++ static/register.js
@@ -1,12 +1,14 @@
 const checkUsername = (event) => {
-    const usernameFormElement = event.target
+    event.preventDefault()
+
+    const usernameFormElement = document.getElementById("username")
     const username = usernameFormElement.value
 
     axios.post('/auth-user', {
         username: username
     })
     .then((response) => {
-        if(response.data.username_exists == 'true') {
+        if(response.data.username_exists === 'true') {
             usernameFormElement.setCustomValidity("This user has already been taken!")
         } else {
             usernameFormElement.setCustomValidity("");
@@ -16,14 +18,16 @@ const checkUsername = (event) => {
 }
 
 const checkEmail = (event) => {
-    const emailFormElement = event.target
+    event.preventDefault()
+
+    const emailFormElement = document.getElementById("email")
     const email = emailFormElement.value
 
     axios.post('/auth-email', {
         email: email
     })
     .then((response) => {
-        if(response.data.email_exists == 'true') {
+        if(response.data.email_exists === 'true') {
             emailFormElement.setCustomValidity("This email has already been taken!")
         } else {
             emailFormElement.setCustomValidity("");
@@ -31,3 +35,24 @@ const checkEmail = (event) => {
         emailFormElement.reportValidity();
     })
 }
+
+function submitAuthentication(event) {
+    event.preventDefault()
+    checkUsername(event)
+    checkEmail(event)
+}
+
+const clearUsernameValidity = () => {
+    const usernameFormElement = document.getElementById("username");
+    usernameFormElement.setCustomValidity("");
+    usernameFormElement.reportValidity();
+};
+
+const clearEmailValidity = () => {
+    const emailFormElement = document.getElementById("email");
+    emailFormElement.setCustomValidity("");
+    emailFormElement.reportValidity();
+};
+
+document.getElementById("username").addEventListener("input", clearUsernameValidity);
+document.getElementById("email").addEventListener("input", clearEmailValidity);
