cab58ecb8febcfc8abd0c50536ada874ade41ef5
Luiz Sa <28lfss@gmail.com>
2025-01-27 23:05:25 -0300

    feat(form): delete login_form code

--- models/login_form.py
+++ models/login_form.py
@@ -1,15 +1,1 @@
-from flask_wtf import FlaskForm
-from wtforms import PasswordField, SubmitField, StringField
-from wtforms.validators import DataRequired, Length
-
-
-class LoginForm(FlaskForm):
-    username = StringField(
-        'Username',
-        validators=[DataRequired(), Length(min=4, max=20)]
-    )
-    password = PasswordField(
-        'Password',
-        validators=[DataRequired(), Length(min=5, max=30)]
-    )
-    submit = SubmitField('Login')
+#TODO: create login form
