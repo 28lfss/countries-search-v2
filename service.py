cd5e49eb5310ee92b0016e6fe88110fbac7a78f5
Luiz Sa <28lfss@gmail.com>
2025-01-30 15:08:07 -0300

    service new http status feature

--- service.py
+++ service.py
@@ -28,3 +28,22 @@ def get_all_countries():
         })
     return jsonify(formated_countries)
 
+def http_status (list):
+    for code in list:
+        match code:
+            case 200:
+                return jsonify({"message": "OK"}), 200
+            case 520:
+                return jsonify({"message": "username wrong patter"}), 520
+            case 521:
+                return jsonify({"message": "username in use"}), 521
+            case 522:
+                return jsonify({"message": "email wrong pattern"}), 522
+            case 523:
+                return jsonify({"message": "email in use"}), 523
+            case 524:
+                return jsonify({"message": "password wrong pattern"}), 524
+            case 525:
+                return jsonify({"message": "passwords must match"}), 525
+            case _:
+                return jsonify({"message": "Invalid"}), 400
