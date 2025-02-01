f350236f921d43ce6bcb6c355b3a5229fc012349
Luiz Sa <28lfss@gmail.com>
2025-02-01 18:09:14 -0300

    feat(app): add encrypted key generation

--- app.py
+++ app.py
@@ -2,6 +2,8 @@ from flask import Flask
 from models.db_models import db
 from api import api
 from page import page
+from Crypto.Cipher import AES
+from Crypto.Random import get_random_bytes
 
 app = Flask(__name__)
 
@@ -10,6 +12,9 @@ app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///database.sqlite3"
 app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
 
 db.init_app(app)
+
+aes_key = get_random_bytes(32)
+cipher = AES.new(aes_key, AES.MODE_CTR)
 
 app.register_blueprint(api)
 app.register_blueprint(page)
