1316f5a14472dc120c0c962477275eb8acddecdf
Luiz Sa <28lfss@gmail.com>
2025-02-01 16:58:01 -0300

    style(repo)

--- repository.py
+++ repository.py
@@ -1,19 +1,19 @@
 from models.db_models import User, Country, user_favorites
 
-def search_id(user_id):
+def get_user_by_id(user_id):
     return User.query.get(user_id)
 
-def search_username(username):
+def get_user_by_username(username):
     return User.query.filter_by(username=username).first()
 
-def check_username(username):
-    return User.query.filter_by(username=username).first() != None
-
-def search_email(email):
+def get_user_by_email(email):
     return User.query.filter_by(email=email).first()
 
+def check_username(username):
+    return User.query.filter_by(username=username).first() is not None
+
 def check_email(email):
-    return User.query.filter_by(email=email).first() != None
+    return User.query.filter_by(email=email).first() is not None
 
 def all_countries():
     return Country.query.all()
