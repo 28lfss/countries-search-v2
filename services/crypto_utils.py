f67f3991687568c2430e51c38d9071386edd9912
Luiz Sa <28lfss@gmail.com>
2025-02-05 20:31:20 -0300

    feat(crypto): user token time validation

--- services/crypto_utils.py
+++ services/crypto_utils.py
@@ -2,8 +2,13 @@ from Crypto.Cipher import AES
 from Crypto.Random import get_random_bytes
 import base64
 import json
+import time
+
+from flask import jsonify
 
 aes_key = get_random_bytes(32)
+
+token_time_limit = 60 * 15 # Calculate 15min in seconds
 
 def generate_user_token(user_json):
     data = json.dumps(user_json)
@@ -13,7 +18,18 @@ def generate_user_token(user_json):
             base64.b64encode(cipher.nonce).decode()) # convert nonce to Base64 to transmit as JSON
 
 def validate_user_token(token, nonce):
-    if nonce is not None:
-        cipher = AES.new(aes_key, AES.MODE_CTR, nonce=nonce)
-        message = cipher.decrypt(token).decode() # Decrypt data
-        return message
+        new_token = base64.b64decode(token)
+        new_nonce = base64.b64decode(nonce)
+
+        cipher = AES.new(aes_key, AES.MODE_CTR, nonce=new_nonce)
+        decrypted_json = json.loads(cipher.decrypt(new_token).decode()) # Decrypt data
+
+        timestamp = int(decrypted_json["timestamp"])
+
+        if (int(time.time()) - timestamp) > token_time_limit:
+            return jsonify({
+                "error": "Token expired",
+                "message": "Log in again for a new token"
+            }), 401
+        else:
+            return jsonify({"status": "OK"}), 200
