7bd2882ce95cafd5bf3fccc0d463ee9edb800eee
Luiz Sa <28lfss@gmail.com>
2025-02-05 12:28:15 -0300

    update(api): token generation on login

--- api/auth.py
+++ api/auth.py
@@ -1,53 +1,80 @@
 from flask import Blueprint, request, jsonify
 from models.register_form import RegisterForm
-from repository import check_username, check_email
-from service import http_status
 from models.db_models import db, User
+from repository import check_username, check_email, get_user_by_username
+from services.auth import get_registration_status
+from services.crypto_utils import generate_user_token, validate_user_token
+import time
 
 auth = Blueprint("auth", __name__)
 
-@auth.route('/register', methods=['POST'])
-def validate_submit():
-    form = request.form
-    if request.method == 'POST':
+@auth.route("/register", methods=["POST"])
+def validate_register_form():
+    if request.method == "POST":
+        form = request.form
         register_form = RegisterForm(
-            username=form['username'],
-            email=form['email'],
-            password=form['password'],
-            confirm_password=form['confirm_password']
+            username=form["username"],
+            email=form["email"],
+            password=form["password"],
+            confirm_password=form["confirm_password"]
         )
         for result in register_form.validate_all():
             if result != 200:
-                return http_status(result)
+                return get_registration_status(result)
 
         user = User(
-            username=form['username'],
-            email=form['email'],
+            username=form["username"],
+            email=form["email"],
         )
-        user.set_password(form['password'])
+        user.set_password(form["password"])
         db.session.add(user)
         db.session.commit()
 
-        return "Usuário registrado"
+        return jsonify({"status": "OK"}), 200
 
-@auth.route('/username', methods=['POST'])
+
+@auth.route("/login", methods=["POST"])
+def validate_login_form():
+    if request.method == "POST":
+
+        form = request.form.to_dict()
+        user = get_user_by_username(form["username"]) if check_username(form["username"]) else None
+
+        if user and user.check_password(form["password"]):
+
+            username = form.get("username")
+            timestamp = f"{int(time.time())}" #get timestamp in seconds
+            data = {"timestamp": timestamp, "username": username}
+            token, nonce = generate_user_token(data)
+
+            return jsonify({
+                "token": token,
+                "nonce": nonce
+            }), 200
+        else:
+            return jsonify({"status": "login unauthorized"}), 401
+
+
+@auth.route("/username", methods=["POST"])
 def auth_user():
-    if request.method == 'POST':
-        data = jsonify({'username_exists': 'false'})
-        username = request.get_json()['username']
+    if request.method == "POST":
+        data = jsonify({"username_exists": "false"})
+        username = request.get_json()["username"]
         if check_username(username):
-            data = jsonify({'username_exists': 'true'})
+            data = jsonify({"username_exists": "true"})
         return data
 
-@auth.route('/email', methods=['POST'])
+
+@auth.route("/email", methods=["POST"])
 def auth_email():
-    if request.method == 'POST':
-        data = jsonify({'email_exists': 'false'})
-        email = request.get_json()['email']
+    if request.method == "POST":
+        data = jsonify({"email_exists": "false"})
+        email = request.get_json()["email"]
         if check_email(email):
-            data = jsonify({'email_exists': 'true'})
+            data = jsonify({"email_exists": "true"})
         return data
 
-@auth.route('/test', methods=['POST'])
-def test():
+
+@auth.route("/test", methods=["GET"])
+def teste():
     return "test"
