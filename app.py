2d08125fd55151cc479ee5a4040e7039123678ef
Luiz Sa <28lfss@gmail.com>
2025-02-11 09:11:21 -0300

    app config and .env model

--- app.py
+++ app.py
@@ -2,17 +2,29 @@ from flask import Flask
 from models.db_models import db
 from api import api
 from page import page
+from flask_mail import Mail
+from dotenv import load_dotenv
+import os
+
+load_dotenv()
 
 app = Flask(__name__)
 
-app.secret_key = "MySecretKey"
+app.config["SECRET_KEY"] = os.getenv("SECRET_KEY")
 app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///database.sqlite3"
 app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
 
+app.config["MAIL_SERVER"] = "smtp.gmail.com"
+app.config["MAIL_PORT"] = 465
+app.config["MAIL_USE_SSL"] = True
+app.config["MAIL_USERNAME"] = os.getenv("MAIL_USERNAME")
+app.config["MAIL_PASSWORD"] = os.getenv("MAIL_PASSWORD")
+
 db.init_app(app)
+mail = Mail(app)
 
 app.register_blueprint(api)
 app.register_blueprint(page)
 
 if __name__ == "__main__":
-    app.run()
+    app.run(debug=True)
